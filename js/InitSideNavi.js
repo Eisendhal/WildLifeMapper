caa3deaff39052629480ae64247a9c5b7b8418c8
Nikoti9510 <antoine.moreau13@laposte.net>
2016-10-16 18:40:57 -0400

    modification de la navigation et ajout d'un tooltip sur chaque element de la liste d'animaux

---
+++ js/InitSideNavi.js
@@ -0,0 +1,9 @@
+jQuery(document).ready( function () {
+	SideNavi.init({
+		container : '#sideNavi',
+		item : '.side-navi-item',
+		data : '.side-navi-data',
+		tab : '.side-navi-tab',
+		active : '.active'
+	});
+});
