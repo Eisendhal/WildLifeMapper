26de114be754a4988dd6a77c7e99b4269c4b6514
Nikoti9510 <antoine.moreau13@laposte.net>
2016-10-17 19:14:22 -0400

    Modification de la navigation et du logo

--- js/InitSideNavi.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready( function () {
-	SideNavi.init({
-		container : '#sideNavi',
-		item : '.side-navi-item',
-		data : '.side-navi-data',
-		tab : '.side-navi-tab',
-		active : '.active'
-	});
-});
---
+++ js/InitSideNaviV2.js
@@ -0,0 +1,5 @@
+    jQuery(document).ready(function($) {
+        $('#sidebar-btn').click(function() {
+            $('#sidebar').toggleClass('visible');
+        });
+    });
